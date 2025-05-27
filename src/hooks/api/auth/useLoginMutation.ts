41b92367f16ff1fcdaef527cb53826950e9ed294
sihyuuun <leesh3597@gmail.com>
2025-05-27 17:48:03 +0900

    fix: build error

--- src/hooks/api/auth/useLoginMutation.ts
+++ src/hooks/api/auth/useLoginMutation.ts
@@ -1,7 +1,1 @@
-import { useMutation } from '@tanstack/react-query';
-import { AuthAPI } from '@/api/authAPI';
-
-export const useLoginMutation = () =>
-  useMutation({
-    mutationFn: (formData: { userId: string; userPwd: string }) => AuthAPI.login(formData),
-  });
+// useLocationMutaion
