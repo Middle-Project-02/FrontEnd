07a813f61627dc5cea23209ff161cebbd25ef80c
neungdong <iyeseul877@gmail.com>
2025-05-31 20:51:25 +0900

    feat: 인증 라우트 보호 컴포넌트 및 라우터 설정 변경 [TOG-9]

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App.tsx';
-import { BrowserRouter } from 'react-router';
 import './index.css';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </QueryClientProvider>,
 );
