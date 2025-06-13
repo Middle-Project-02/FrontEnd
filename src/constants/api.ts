882b7b469e5376295ca0799ba71523b5c7f146d4
0Jaemin0 <jaemin2167@naver.com>
2025-06-13 09:50:04 +0900

    feat: auth 관련 메서드 구현 #6

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,7 +1,19 @@
 export const BASE_URL = import.meta.env.VITE_BASE_URL;
 
+export const END_POINTS = {
+  LOGIN: 'auth/login',
+  REISSUE: 'auth/reissue',
+  USERINFORMATION: 'members/me',
+} as const;
+
 export const NETWORK_TIMEOUT = 30000;
 
 export const HTTP_STATUS_CODE = {
+  UNAUTHORIZED: 401,
   INTERNAL_SERVER_ERROR: 500,
+} as const;
+
+export const AUTH_ERROR_CODE = {
+  EXPIRED_ACCESS_TOKEN: 40116,
+  EXPIRED_REFRESH_TOKEN: 40118,
 } as const;
