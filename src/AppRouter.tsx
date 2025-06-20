9d4994edcca27bfe09122e01a7b1a62111eac934
0Jaemin0 <jaemin2167@naver.com>
2025-06-20 11:30:21 +0900

    feat: mainpage path 추가에 따른 라우터 반영 및 수정 #32

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -4,37 +4,45 @@ import BaseLayout from '@/components/layout/base/BaseLayout';
 import MainPage from '@/pages/MainPage';
 import LoginPage from '@/pages/LoginPage';
 import SmishingPage from '@/pages/SmishingPage';
-import { requireAuth, redirectIfAuthenticated } from '@/utils/authLoader';
+import LandingPage from '@/pages/LandingPage';
 import NotificationPage from '@/pages/NotificationPage';
 import NotificationDetailPage from '@/pages/NotificationDetailPage';
+import { requireAuth, redirectIfAuthenticated } from '@/utils/authLoader';
 
 const AppRouter = () => {
   const router = createBrowserRouter([
     {
       path: PATH.ROOT,
-      element: <BaseLayout />,
-      children: [
-        {
-          path: '',
-          element: <MainPage />,
-          loader: requireAuth,
-        },
-        {
-          path: PATH.SMISHING,
-          element: <SmishingPage />,
-          loader: requireAuth,
-        },
-        { path: PATH.NOTIFICATIONS, element: <NotificationPage /> },
-        {
-          path: PATH.NOTIFICATION_DETAIL,
-          element: <NotificationDetailPage />,
-        },
-      ],
+      element: <LandingPage />,
     },
     {
       path: PATH.LOGIN,
       element: <LoginPage />,
       loader: redirectIfAuthenticated,
+    },
+    {
+      path: '',
+      element: <BaseLayout />,
+      children: [
+        {
+          path: PATH.SMISHING,
+          element: <SmishingPage />,
+          loader: requireAuth,
+        },
+        {
+          path: PATH.NOTIFICATIONS,
+          element: <NotificationPage />,
+        },
+        {
+          path: PATH.NOTIFICATION_DETAIL,
+          element: <NotificationDetailPage />,
+        },
+        {
+          path: PATH.MAIN,
+          element: <MainPage />,
+          loader: requireAuth,
+        },
+      ],
     },
   ]);
 
