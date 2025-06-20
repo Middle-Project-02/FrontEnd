08be03ec7aaf895ab0c5a3505ebaac4be0afafb2
sihyuuun <leesh3597@gmail.com>
2025-06-20 11:36:57 +0900

    fix: assets/svg/index.ts 에러 수정 #23

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -4,4 +4,3 @@ export { default as Issue } from './alert-circle.svg';
 export { default as Impact } from './help-circle.svg';
 export { default as Solution } from './check-circle-2.svg';
 export { default as LoadingDog } from './loading-dog.svg';
-export { default as NoDataDog } from './no-data-dog.svg';
