3ae62a1346364bf4b634339cb5e549cb48b00457
0Jaemin0 <jaemin2167@naver.com>
2025-06-23 03:11:38 +0900

    feat: 글씨 모드 상태에 따른 font-size 변경 #50

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import AppRouter from './AppRouter';
+import { useEffect } from 'react';
 import ModalContainer from '@/components/common/ModalContainer';
 import { Toaster } from '@/components/ui/sonner';
+import useFontModeStore from '@/stores/fontModeStore';
+import AppRouter from './AppRouter';
 
 function App() {
+  const { fontMode } = useFontModeStore();
+
+  useEffect(() => {
+    const html = document.documentElement;
+
+    if (fontMode) {
+      html.classList.add('large-text');
+    } else {
+      html.classList.remove('large-text');
+    }
+  }, [fontMode]);
+
   return (
     <>
       <AppRouter />
