6b81f6b849c071388d3e64e644b9310011b07d24
0Jaemin0 <jaemin2167@naver.com>
2025-06-23 03:16:30 +0900

    feat: 홈 화면 서비스 카테고리 버튼 구현 #50

---
+++ src/components/home/CategoryButton.tsx
@@ -0,0 +1,40 @@
+import { ICON_MAP } from '@/constants/home';
+
+interface CategoryButtonProps {
+  fontMode: boolean;
+  title: string;
+  iconName: string;
+  fullWidth?: boolean;
+  onClick?: () => void;
+}
+
+const CategoryButton = ({ fontMode, title, iconName, fullWidth, onClick }: CategoryButtonProps) => {
+  const iconSrc = ICON_MAP[iconName as keyof typeof ICON_MAP];
+  const heightClass = fullWidth ? (fontMode ? 'h-[140px]' : 'h-[90px]') : 'aspect-square';
+
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={`relative w-full ${heightClass} rounded-16 border border-borderSecondary p-16 shadow-shadow2 text-left ${
+        fontMode && 'flex flex-col items-center justify-center'
+      }`}
+    >
+      {fontMode ? (
+        <>
+          <img src={iconSrc} alt="icon" className="w-[70px] h-[70px] mb-4" />
+          <p className="text-body-lg font-bold text-black text-center">{title}</p>
+        </>
+      ) : (
+        <>
+          <p className="absolute top-16 left-16 right-16 text-body-md font-bold text-black leading-[1.1] ">
+            {title}
+          </p>
+          <img src={iconSrc} alt="icon" className="absolute bottom-12 right-16 w-[30px] h-[30px]" />
+        </>
+      )}
+    </button>
+  );
+};
+
+export default CategoryButton;
