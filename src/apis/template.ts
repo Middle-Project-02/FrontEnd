88ca1de27ea198912c2303df6426a159f8856566
sihyuuun <leesh3597@gmail.com>
2025-06-22 18:05:50 +0900

    chore: 절대 경로로 수정 및 import type 추가 #18

--- src/apis/template.ts
+++ src/apis/template.ts
@@ -1,5 +1,5 @@
-import { axiosInstance } from './axiosInstance';
-import { Template } from '@/types/template';
+import { axiosInstance } from '@/apis/axiosInstance';
+import type { Template } from '@/types/template';
 import { END_POINTS } from '@/constants/api';
 
 export const getTemplates = async (): Promise<Template[]> => {
@@ -12,7 +12,7 @@ export const getTemplateDetail = async (id: number): Promise<Template> => {
   return response.data.content;
 };
 
-export const saveTemplate = async (data: { title: string; content: string }): Promise<void> => {
+export const postSaveTemplate = async (data: { title: string; content: string }): Promise<void> => {
   await axiosInstance.post(END_POINTS.TEMPLATE.SAVE, data, {
     headers: { 'Content-Type': 'application/json' },
   });
