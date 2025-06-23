b8ecb4320f9e0565ccdaa518797087079d6a8dcc
0Jaemin0 <jaemin2167@naver.com>
2025-06-23 14:15:48 +0900

    feat: skeleton UI 반영을 위한 요금제 상세 조회 isLoading 추가 #50

--- src/hooks/queries/ranking/useRankDetailQuery.ts
+++ src/hooks/queries/ranking/useRankDetailQuery.ts
@@ -1,13 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 import { getRankingDetail } from '@/apis/ranking';
-import { RankingPlanDetailResponse } from '@/types/ranking';
+import type { RankingPlanDetailResponse } from '@/types/ranking';
 
 const useRankDetailQuery = (rankId: number | null) => {
-  const { data: rankDetailResponse } = useQuery<RankingPlanDetailResponse>({
+  const { data: rankDetailResponse, isLoading } = useQuery<RankingPlanDetailResponse>({
     queryKey: ['rankingDetail', rankId],
     queryFn: () => getRankingDetail(rankId!),
+    enabled: !!rankId,
   });
-  return { rankDetailResponse };
+
+  return { rankDetailResponse, isLoading };
 };
 
 export default useRankDetailQuery;
