0ef419543321dd6813ad9e80171563869b44ed8a
ihyeeun <oioigl@naver.com>
2025-06-24 01:11:08 +0900

    feat: ranking list 데이터 연결 #65

--- src/components/ranking/FirstPlanCard.tsx
+++ src/components/ranking/FirstPlanCard.tsx
@@ -1,9 +1,12 @@
-const FirstPlanCard = () => {
+import type { RankingPlanSummary } from '@/types/ranking';
+
+const FirstPlanCard = (plan: { plan: RankingPlanSummary }) => {
   return (
     <div className="bg-primary-gradient rounded-16 px-36 py-16 shadow-shadowP2 -rotate-2 text-white aspect-[4/3] w-full">
       <div className="flex flex-col justify-center w-full h-full">
-        <div className="text-heading-h4 font-bold mb-2">데이터</div>
-        <div className="text-heading-h4 text-textPrimary">무제한</div>
+        <p className="text-heading-h4 font-bold mb-2">데이터</p>
+        <p className="text-heading-h4 text-textPrimary">{plan.plan.dataAmount}</p>
+        {plan.plan.speedLimit && <p className="text-body-sm">+ {plan.plan.speedLimit}</p>}
       </div>
     </div>
   );
--- src/components/ranking/RegulerPlanCard.tsx
+++ src/components/ranking/RegulerPlanCard.tsx
@@ -1,9 +1,11 @@
-const RegulerPlanCard = () => {
+import type { RankingPlanSummary } from '@/types/ranking';
+
+const RegulerPlanCard = (plan: { plan: RankingPlanSummary }) => {
   return (
-    <div className="bg-[#676767] rounded-16 shadow-shadow8 text-white aspect-[4/3] w-full h-full">
+    <div className="bg-[#676767] rounded-16 shadow-shadow8 text-white aspect-[4/3] w-full h-full -rotate-0">
       <div className="flex flex-col justify-center items-center w-full h-full">
-        <div className="text-body-lg font-bold mb-2">데이터</div>
-        <div className="text-body-md text-textPrimary">100GB</div>
+        <p className="text-body-md font-semibold">{plan.plan.dataAmount}</p>
+        {plan.plan.speedLimit && <p className="text-body-sm">+ {plan.plan.speedLimit}</p>}
       </div>
     </div>
   );
