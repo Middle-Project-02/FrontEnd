5b6f40eaa3946b07ad22b1810a73a74a40a337fd
ihyeeun <oioigl@naver.com>
2025-06-24 02:07:01 +0900

    feat: ranking detail page 추가 및 연결 #65

---
+++ src/pages/ranking/RankingDetailPage.tsx
@@ -0,0 +1,29 @@
+// pages/RankingDetailPage.tsx
+import { useParams, useNavigate } from 'react-router-dom';
+import PlanDetailItem from '@/components/ranking/PlanDetailItem';
+import { PATH } from '@/constants/path';
+
+const RankingDetailPage = () => {
+  const { id } = useParams<{ id: string }>();
+  const navigate = useNavigate();
+
+  const planId = id ? parseInt(id, 10) : null;
+
+  const handleBack = () => {
+    navigate(`${PATH.RANKING.MAIN}`); // 목록 페이지로 돌아가기
+  };
+
+  if (!planId) {
+    return <div>잘못된 접근입니다.</div>;
+  }
+
+  return (
+    <div className="flex flex-col w-full h-full pt-44">
+      <div className="px-30 h-full overflow-y-auto">
+        <PlanDetailItem planId={planId} onBack={handleBack} />
+      </div>
+    </div>
+  );
+};
+
+export default RankingDetailPage;
