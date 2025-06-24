97deed8696b75c343b32ac50d1850bff4c736229
ihyeeun <oioigl@naver.com>
2025-06-24 14:50:55 +0900

    feat: ranking 단계별 페이지 분리 #65

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -4,7 +4,8 @@ import BaseLayout from '@/components/layout/base/BaseLayout';
 import LoginPage from '@/pages/LoginPage';
 import LandingPage from '@/pages/LandingPage';
 import SmishingPage from '@/pages/smishing/SmishingPage';
-import RankingPage from '@/pages/ranking/RankingPage';
+import RankingAgeSelectPage from '@/pages/ranking/RankingAgeSelectPage';
+import RankingListPage from '@/pages/ranking/RankingListPage';
 import RankingDetailPage from '@/pages/ranking/RankingDetailPage';
 import SmishingIntroPage from '@/pages/smishing/SmishingIntroPage';
 import PlanChatBotPage from '@/pages/PlanChatBotPage';
@@ -107,8 +108,12 @@ const AppRouter = () => {
         { path: PATH.TEMPLATES, element: <TemplatesPage />, loader: requireAuth },
         { path: PATH.TEMPLATE_DETAIL, element: <TemplateDetailPage />, loader: requireAuth },
         {
-          path: PATH.RANKING.MAIN,
-          element: <RankingPage />,
+          path: PATH.RANKING.AGE_SELECT,
+          element: <RankingAgeSelectPage />,
+        },
+        {
+          path: PATH.RANKING.LIST,
+          element: <RankingListPage />,
         },
         {
           path: PATH.RANKING.DETAIL,
