51f8142fd95ffbdde79ab38f71e56bdf4c61bde2
6v6DS <142214903+6v6DS@users.noreply.github.com>
2025-06-25 21:23:22 +0000

    Update api.ts

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_BASE_URL;
+export const BASE_URL = "https://f649-3-239-33-213.ngrok-free.app/api";
 
 export const END_POINTS = {
   LOGIN: 'auth/login',
