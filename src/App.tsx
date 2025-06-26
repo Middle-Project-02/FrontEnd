e6bbd4e65ac68842c5c8a92379fb624b5b4e3d1f
0Jaemin0 <jaemin2167@naver.com>
2025-06-26 18:11:36 +0900

    fix: pwa 환경 임시 제거

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,11 @@ import { FirebaseProvider } from '@/components/providers/FirebaseProvider';
 import ModalContainer from '@/components/common/ModalContainer';
 import { Toaster } from '@/components/ui/sonner';
 import useFontModeStore from '@/stores/fontModeStore';
-import { registerSW } from 'virtual:pwa-register';
 
 function App() {
   const { fontMode } = useFontModeStore();
 
   useEffect(() => {
-    registerSW();
-
     const html = document.documentElement;
 
     if (fontMode) {
--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,10 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'prompt',
-      filename: 'sw.js',
-      includeAssets: ['favicon.ico'],
-      manifest: {
-        name: 'together',
-        short_name: 'together',
-        description: '2조종합프로젝트',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        start_url: '/',
-        icons: [
-          {
-            src: 'icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-          {
-            src: 'icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
