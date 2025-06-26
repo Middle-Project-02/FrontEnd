3274282b6576da2ffde9d9d6c5936e0822c27a05
0Jaemin0 <jaemin2167@naver.com>
2025-06-26 18:35:14 +0900

    fix: firebase 코드 주석 처리

--- src/hooks/useFirebaseMessaging.ts
+++ src/hooks/useFirebaseMessaging.ts
@@ -1,12 +1,12 @@
-import { useEffect } from 'react';
-import { requestPermissionAndGetToken } from '@/utils/requestPermissionAndGetToken';
+// import { useEffect } from 'react';
+// import { requestPermissionAndGetToken } from '@/utils/requestPermissionAndGetToken';
 
-export const useFirebaseMessaging = () => {
-  useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/firebase-messaging-sw.js');
-    }
+// export const useFirebaseMessaging = () => {
+//   useEffect(() => {
+//     if ('serviceWorker' in navigator) {
+//       navigator.serviceWorker.register('/firebase-messaging-sw.js');
+//     }
 
-    requestPermissionAndGetToken();
-  }, []);
-};
+//     requestPermissionAndGetToken();
+//   }, []);
+// };
--- src/utils/requestPermissionAndGetToken.ts
+++ src/utils/requestPermissionAndGetToken.ts
@@ -1,19 +1,19 @@
-import { sendFcmToken } from '@/apis/user';
-import { messaging } from './firebase';
-import { getToken } from 'firebase/messaging';
+// import { sendFcmToken } from '@/apis/user';
+// import { messaging } from './firebase';
+// import { getToken } from 'firebase/messaging';
 
-export const requestPermissionAndGetToken = async () => {
-  const permission = await Notification.requestPermission();
+// export const requestPermissionAndGetToken = async () => {
+//   const permission = await Notification.requestPermission();
 
-  if (permission === 'granted') {
-    const registration = await navigator.serviceWorker.ready;
-    const token = await getToken(messaging, {
-      vapidKey: import.meta.env.VITE_FIREBASE_VAPID_KEY,
-      serviceWorkerRegistration: registration,
-    });
+//   if (permission === 'granted') {
+//     const registration = await navigator.serviceWorker.ready;
+//     const token = await getToken(messaging, {
+//       vapidKey: import.meta.env.VITE_FIREBASE_VAPID_KEY,
+//       serviceWorkerRegistration: registration,
+//     });
 
-    if (token) {
-      await sendFcmToken(token);
-    }
-  }
-};
+//     if (token) {
+//       await sendFcmToken(token);
+//     }
+//   }
+// };
